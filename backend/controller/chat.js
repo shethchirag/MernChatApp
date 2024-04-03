b11bb9d72f27fea09ac17cbe1eec4314ca2ba745
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-04-03 20:05:02 +0530

    addmembers to group..

--- backend/controller/chat.js
+++ backend/controller/chat.js
@@ -4,6 +4,7 @@ import { ALERT, REFETCH_CHAT } from "../constant/event.js";
 import { getOtherMember } from "../lib/helper.js";
 import { TryCatch } from "../middleware/error.js";
 import { Chat } from "./../models/chat.js";
+import { User } from "./../models/user.js";
 
 const newGroupChat = TryCatch(async (req, res, next) => {
   const { name, members } = req.body;
@@ -80,4 +81,50 @@ const getMyGroups = TryCatch(async (req, res, next) => {
   });
 });
 
-export { newGroupChat, getMyChats, getMyGroups };
+const addMembers = TryCatch(async (req, res, next) => {
+  const { chatId, members } = req.body;
+
+  if (!members || members.length < 1) {
+    return next(new ErrorHandler("please provide members", 400));
+  }
+  const chat = await Chat.findById(chatId);
+
+  if (!chat) return next(new ErrorHandler("chat not found", 404));
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat", 400));
+
+  if (chat.creator.toString() !== req.user.toString()) {
+    return next(new ErrorHandler("you are not allowed to add members", 403));
+  }
+
+  const allNewMembersPromise = members.map((i) => User.findById(i, "name"));
+  const allMembers = await Promise.all(allNewMembersPromise);
+  const uniqueMembers = allMembers
+    .filter((i) => !chat.members.includes(i._id.toString()))
+    .map((i) => i._id);
+  chat.members.push(...uniqueMembers);
+
+  if (chat.members.length > 100) {
+    return next(
+      new ErrorHandler("group can not have more than 100 members", 400)
+    );
+  }
+  await chat.save();
+  const allUsersName = allMembers.map((i) => i.name).join(",");
+
+  emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${allUsersName} has been added in group `
+  );
+
+  emitEvent(req, REFETCH_CHAT, chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "added member successfully",
+  });
+});
+
+export { newGroupChat, getMyChats, getMyGroups, addMembers };
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
-import { getMyChats, getMyGroups, newGroupChat } from "../controller/chat.js";
+import {
+  addMembers,
+  getMyChats,
+  getMyGroups,
+  newGroupChat,
+} from "../controller/chat.js";
 
 const app = express.Router();
 
@@ -9,5 +14,6 @@ app.use(isAuthenticated);
 app.post("/new", newGroupChat);
 app.get("/my", getMyChats);
 app.get("/my/group", getMyGroups);
+app.put("/addmembers", addMembers);
 
 export default app;
